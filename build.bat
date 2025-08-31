679fd1bc5e2522bdd6af63eb09b3881595a3e7fd
NeuraXmy <neuraXmy@outlook.com>
2025-08-31 15:48:24 +0800

    修改使用帮助文件名

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 pyinstaller --name "nightreign-float-timer" --windowed --icon="assets\icon.ico" src\app.py
 xcopy /E /I /Y "assets" "dist\nightreign-float-timer\assets"
-copy "README.txt" "dist\nightreign-float-timer\README.txt"
+copy "manual.txt" "dist\nightreign-float-timer\使用帮助.txt"
 copy "config.yaml" "dist\nightreign-float-timer\config.yaml"
